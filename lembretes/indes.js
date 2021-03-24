9cec29e78d598c1e8007fd69fe57346b1dd1c1ce
João Victor Cariolano Silva <joao.victorcoriolano@gmail.com>
2021-03-24 12:22:26 -0300

    Criacao inicial do mss de lembretes

---
+++ lembretes/indes.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express();
+
+//https://localhost:porta/lembretes
+app.get('/lembretes', (req, res)=> {
+    
+})
+
+app.put('/lembretes', (req, res) => {
+
+} )
+
+app.listen(4000, () => console.log("Lembretes:4000."))
